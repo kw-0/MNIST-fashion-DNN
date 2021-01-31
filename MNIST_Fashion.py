3517f0c4d6ed11bc7bda5cb488b09786ddc80642
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-01-31 10:03:12 -0500

    Update MNIST_Fashion.py

--- MNIST_Fashion.py
+++ MNIST_Fashion.py
@@ -6,20 +6,26 @@ import random
 raw_train = pd.read_csv("/Users/kylenwilliams/Fashion MNIST/fashion-mnist_train.csv")   # Your path to data file
 raw_test = pd.read_csv("/Users/kylenwilliams/Fashion MNIST/fashion-mnist_test.csv")     # Your path to data file
 
-# note to self; don't forget to randomize and batch the data
+# note to self; don't forget to randomize data
 train_data = np.array(raw_train)    # train data array
 test_data = np.array(raw_test)      # test data array
+
+E = np.e
 
 
 class DenseLayer:
     def __init__(self, nrn_inputs, nrn_num):
-        self.weights = 0.01 * random.randint(len(nrn_inputs), nrn_num)
-        self.biases = np.zeros((1, len(nrn_inputs)))
+        self.weights = 0.01 * np.random.randn(nrn_inputs, nrn_num)
+        self.biases = np.zeros((1, nrn_num))
 
     def forward_pass(self, inputs):
         self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
         return self.output
+
+    def backward_pass(self):
+        pass
+
 
     # def backward_pass(self, inputs, weights):
     #     self.dinputs = inputs
@@ -27,22 +33,40 @@ class DenseLayer:
 
 
 class ReLuActivation:
-    def forward(self, inputs):
+    def forward_pass(self, inputs):
         self.input = inputs
         self.output = np.maximum(0, inputs)
         return self.output
+
+    def backward_pass(self):
+        pass
+
+
+class SigmoidActivation:
+    def forward_pass(self, inputs):
+        exp_values = E ** (inputs - np.max(inputs, axis=1, keepdims=True))  # sub by max to keep the vals from exploding
+        normalized_exp_vals = exp_values / np.sum(exp_values)
+        self.output = normalized_exp_vals
+        return self.output
+
+    def backward_pass(self):
+        pass
+
+
+# class Loss:
 
 
 # just some testing and messing with the class and forward pass:
 clothing_idx = random.randint(0, 60000)
 print(f"Index of chosen clothing item: {clothing_idx}")
 
-input_layer = DenseLayer(train_data[clothing_idx], 785)
-input_layer_out = input_layer.forward_pass(train_data[clothing_idx])
+input_layer = DenseLayer(785, 800)
+input_layer.forward_pass(train_data[clothing_idx])
+actviation_lay1 = SigmoidActivation()
 
-r = ReLuActivation().forward(input_layer_out)
-print(r)
-
-# img = train_data[clothing_idx, 0:784].reshape((28, 28))    # reshaping the flattened array to a 28x28 so plt can show it
-# pltp.imshow(img, cmap="gray")
+# img = train_data[clothing_idx, 0:784].reshape((28, 28))
+# img_r = r[0, 0:784].reshape((28, 28))    # reshaping the flattened array to a 28x28 so plt can show it
+# print(r)
+# pltp.imshow(img_r, cmap="gray")
+# # pltp.imshow(img, cmap="gray")
 # pltp.show()
