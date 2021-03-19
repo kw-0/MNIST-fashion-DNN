95f08fb71977b86e2e658ec5ba308715207464e9
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-03-19 11:56:54 -0400

    Housekeeping, optimizing, saving before editing further
    I showed my code to someone and they were like 'um why are you iterating over every batch' and i was like 'oh crap you're right that defeats the whole purpose of batching' so i fixed that. Also removed some of the useless fluff like the choice dict and stuff. I'm still trying to figure out why it only wants to predict one thing every epoch.

--- MNIST_Fashion.py
+++ MNIST_Fashion.py
@@ -1,18 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as pltp
+# import matplotlib.pyplot as pltp
 import numpy as np
 
-raw_train = pd.read_csv('/Users/kylenwilliams/Fashion MNIST/fashion-mnist_train.csv')  # Your path to data file
-raw_test = pd.read_csv('/Users/kylenwilliams/Fashion MNIST/fashion-mnist_test.csv')  # Your path to data file
+raw_train = pd.read_csv('/Users/kylenwilliams/Fashion MNIST/fashion-mnist_train.csv')  # your path to data file
+raw_test = pd.read_csv('/Users/kylenwilliams/Fashion MNIST/fashion-mnist_test.csv')  # your path to data file
+
 
 train_data = np.array(raw_train)  # train data array
 test_data = np.array(raw_test)  # test data array
 
+np.random.shuffle(train_data)
+np.random.shuffle(test_data)
+
 labels = ['T-Shirt', 'Trousers', 'Pullover', 'Dress', 'Coat',
           'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
 
-E = np.e
-EPOCHS = 5
+EPOCHS = 1000
 BATCH_SIZE = 8
 CLIPPER = 1e-7
 
@@ -58,7 +61,8 @@ class ReLuActivation:
 
 class SoftmaxActivation:
     def forward_pass(self, inputs):
-        exp_values = E ** (inputs - np.max(inputs, axis=1, keepdims=True))  # sub by max to keep the vals from exploding
+        # sub by max to keep the values from exploding
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         normalized_exp_vals = exp_values / np.sum(exp_values)
         self.output = normalized_exp_vals
         return self.output  # without the return I had some NoneType errors so this fixed that
@@ -108,7 +112,7 @@ class CategoricalCrossEntropyLoss(GeneralLoss):
         if len(y_true.shape) == 1:  # if sparse
             correct_confidences = y_pred_clipped[
                 range(samples),
-                y_true
+                y_true[range(samples)]
             ]
 
         elif len(y_true.shape) == 2:  # if one-hot
@@ -158,14 +162,74 @@ class ActivationSoftmaxLossCategoricalCrossEntropy:
         self.dinputs = self.dinputs / samples
 
 
-class SGDOptimizer:
-    def __init__(self, learning_rate=1.0):
+class AdamOptimizer:
+    # Initialize optimizer - set settings
+    def __init__(self, learning_rate=0.0001, decay=0., epsilon=1e-7,
+                 beta_1=0.9, beta_2=0.999):
         self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+    # Call once before any parameter updates
+    def pre_optimize(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * \
+                                         (1. / (1. + self.decay * self.iterations))
 
     def optimize(self, layer):
-        # negative because derivative tells direction of steepest ascent so opposite of that leads to minimum
-        layer.weights += -self.learning_rate * layer.dweights
-        layer.biases += -self.learning_rate * layer.dbiases
+
+        # If layer does not contain cache arrays,
+        # create them filled with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update momentum  with current gradients
+        layer.weight_momentums = self.beta_1 * \
+                                 layer.weight_momentums + \
+                                 (1 - self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * \
+                               layer.bias_momentums + \
+                               (1 - self.beta_1) * layer.dbiases
+        # Get corrected momentum
+        # self.iteration is 0 at first pass
+        # and we need to start with 1 here
+        weight_momentums_corrected = layer.weight_momentums / \
+                                     (1 - self.beta_1 ** (self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / \
+                                   (1 - self.beta_1 ** (self.iterations + 1))
+        # Update cache with squared current gradients
+        layer.weight_cache = self.beta_2 * layer.weight_cache + \
+                             (1 - self.beta_2) * layer.dweights ** 2
+
+        layer.bias_cache = self.beta_2 * layer.bias_cache + \
+                           (1 - self.beta_2) * layer.dbiases ** 2
+        # Get corrected cache
+        weight_cache_corrected = layer.weight_cache / \
+                                 (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / \
+                               (1 - self.beta_2 ** (self.iterations + 1))
+
+        # Vanilla SGD parameter update + normalization
+        # with square rooted cache
+        layer.weights += -self.current_learning_rate * \
+                         weight_momentums_corrected / \
+                         (np.sqrt(weight_cache_corrected) +
+                          self.epsilon)
+        layer.biases += -self.current_learning_rate * \
+                        bias_momentums_corrected / \
+                        (np.sqrt(bias_cache_corrected) +
+                         self.epsilon)
+
+    # Call once after any parameter updates
+    def post_update_params(self):
+        self.iterations += 1
 
 
 # fun colors to make reading the output a little easier
@@ -174,6 +238,14 @@ class Color:
     BOLD = '\033[1m'
     END = '\033[0m'
 
+
+ground_truths = []
+for data_point in train_data:
+    ground_truths = np.append(ground_truths, [data_point[0]], 0)     # adds truth values to ground true array
+ground_truths = ground_truths.astype(int)
+# ground_truths = ground_truths.reshape((7500, BATCH_SIZE))
+
+train_data = np.delete(train_data, 0, axis=1)  # deletes all the truths from input data
 
 # instantiating (what a weird word) the neural net
 input_layer = DenseLayer(784, 784)
@@ -189,92 +261,51 @@ activation_lay4 = ReLuActivation()
 output_layer = DenseLayer(64, 10)
 softmax_activation_loss = ActivationSoftmaxLossCategoricalCrossEntropy()
 
-optimizer = SGDOptimizer()
-
+optimizer = AdamOptimizer(decay=5e-7)
 
 for EPOCH in range(EPOCHS):
-    clothing_idx = np.random.randint(0, train_data.shape[0] - BATCH_SIZE)  # -BATCH_SIZE because if it chooses like
-    # 5999, there aren't ten more after it so it can't calculate anything with that
+    # minus BATCH_SIZE because if it chooses like 5999,
+    # there aren't ten more after it so it can't calculate anything with that
+    clothing_idx = np.random.randint(0, train_data.shape[0] - BATCH_SIZE)
 
-    # resets it every epoch
-    choice_frequency_dict = {'T-Shirt': 0, 'Trousers': 0, 'Pullover': 0, 'Dress': 0, 'Coat': 0,
-                             'Sandal': 0, 'Shirt': 0, 'Sneaker': 0, 'Bag': 0, 'Ankle Boot': 0}
+    batch_data = train_data[clothing_idx:clothing_idx + BATCH_SIZE]
+    batch_truths = ground_truths[clothing_idx:clothing_idx + BATCH_SIZE]
 
-    # # puts all the truths into an array
-    ground_truths = []
-    # I did this so I can safely modify the inputs
-    train_data_4_batch = train_data[clothing_idx:clothing_idx + BATCH_SIZE]
+    # forward pass
+    input_layer.forward_pass(batch_data)
+    activation_lay1.forward_pass(input_layer.output)
 
-    count = 0
-    # I was going to convert train data to a list then .pop()
-    # but .pop() only takes one value but we need to index 2, the sample and the truth value (which is at idx 0)
-    for data_point in train_data_4_batch:
-        ground_truths = np.append(ground_truths, [data_point[0]], 0)     # adds truth values to ground true array
+    layer_2.forward_pass(activation_lay1.output)
+    activation_lay2.forward_pass(layer_2.output)
 
-    train_data_4_batch = np.delete(train_data_4_batch, 0, axis=1)  # deletes all the truths from input data
-    ground_truths = ground_truths.astype(int)
+    layer_3.forward_pass(activation_lay2.output)
+    activation_lay3.forward_pass(layer_3.output)
 
-    print(Color.BLUE + f'Epoch {EPOCH + 1}:' + Color.END)
+    layer_4.forward_pass(activation_lay3.output)
+    activation_lay4.forward_pass(layer_4.output)
+
+    output_layer.forward_pass(activation_lay4.output)
+
+    softmax_activation_loss.forward_pass(output_layer.output, batch_truths)
+    loss = softmax_activation_loss.forward_pass(output_layer.output, batch_truths)
+
     for BATCH in range(BATCH_SIZE):
-
-        # truth value for our specific data point
-        true_idx = ground_truths[BATCH]
-        true_label = labels[true_idx]
-
-        # forward pass
-        input_layer.forward_pass(train_data_4_batch)
-        activation_lay1.forward_pass(input_layer.output)
-
-        layer_2.forward_pass(activation_lay1.output)
-        activation_lay2.forward_pass(layer_2.output)
-
-        layer_3.forward_pass(activation_lay2.output)
-        activation_lay3.forward_pass(layer_3.output)
-
-        layer_4.forward_pass(activation_lay3.output)
-        activation_lay4.forward_pass(layer_4.output)
-
-        output_layer.forward_pass(activation_lay4.output)
-
-        loss = softmax_activation_loss.forward_pass(output_layer.output, ground_truths)
-
-        print('Index of clothing item:', clothing_idx)
-
         # finds which the neural net predicted
-        print(softmax_activation_loss.output)
-        prediction = np.argmax(softmax_activation_loss.output)
+        prediction = np.argmax(softmax_activation_loss.output[BATCH])
         prediction_label = labels[int(prediction)]
-        prob_dist = softmax_activation_loss.output[BATCH]
+        print(f'Batch {BATCH + 1}- guess: {prediction_label} truth: {labels[int(batch_truths[BATCH])]}')
+        prob_dist = softmax_activation_loss.output
+    print('')
 
-        print(Color.BOLD + f'{BATCH + 1}- ' + Color.END + f'Guess: {prediction_label}, Truth: {true_label}')
-        # print(f'Probability distribution across labels: \n {prob_dist}\n')
-
-        choice_frequency_dict.update({prediction_label: choice_frequency_dict[prediction_label] + 1})
-
-        # accuracy calc
-        # converts data to sparse if one hot again, our data is sparse but I gotta have that delicious scalability
-        if len(ground_truths.shape) == 2:
-            ground_truths = np.argmax(ground_truths, axis=1)
-        accuracy = np.mean(prediction == ground_truths) * 100
-
-        clothing_idx += 1
-
-    choice_probability_dict = choice_frequency_dict.copy()
-
-    # updates dict of labels with percentages of how often they are chosen
-    total_guesses = sum(choice_probability_dict.values())
-    for label, val in choice_frequency_dict.items():
-        choice_probability_dict.update({label: f'{(val / total_guesses) * 100}%'})
+    # accuracy calc
+    if len(ground_truths.shape) == 2:
+        ground_truths = np.argmax(ground_truths, axis=1)
+    accuracy = np.mean(prediction == ground_truths) * 100
 
     print('\nAverage loss for this batch: ', loss)
-    print(f'Accuracy: {accuracy}%')
-
-    print('Choice frequency: ', choice_frequency_dict)
-    # in the end this should be about even because the data has an equal number of each clothing type
-    print(f'Choice probabilistic frequency: {choice_probability_dict}\n')
 
     # backward pass is after loss and stuff because it needs to change for next layer
-    softmax_activation_loss.backward_pass(softmax_activation_loss.output, ground_truths)
+    softmax_activation_loss.backward_pass(softmax_activation_loss.output, batch_truths)
     output_layer.backward_pass(softmax_activation_loss.dinputs)
 
     activation_lay4.backward_pass(output_layer.dinputs)
@@ -289,15 +320,26 @@ for EPOCH in range(EPOCHS):
     activation_lay1.backward_pass(layer_2.dinputs)
     input_layer.backward_pass(activation_lay1.dinputs)
 
+    optimizer.pre_optimize()
     optimizer.optimize(input_layer)
+    optimizer.post_update_params()
+    optimizer.pre_optimize()
     optimizer.optimize(layer_2)
+    optimizer.post_update_params()
+    optimizer.pre_optimize()
     optimizer.optimize(layer_3)
+    optimizer.post_update_params()
+    optimizer.pre_optimize()
     optimizer.optimize(layer_4)
+    optimizer.post_update_params()
+    optimizer.pre_optimize()
     optimizer.optimize(output_layer)
+    optimizer.post_update_params()
 
-    # if you're on the last epoch show that img
-    if EPOCH+1/EPOCHS == 1:
-        # its 1:785 b/c the 0th val is the ground truth
-        img = train_data[clothing_idx, 1:785].reshape((28, 28))   # reshaping the flattened array to so plt can show it
-        pltp.imshow(img, cmap='gray')
-        pltp.show()
+    print(f'Accuracy: {accuracy}%')
+
+    # # if you're on the last epoch show that img
+    # if EPOCH+1/EPOCHS == 1:
+    #     img = train_data[clothing_idx, 1:784].reshape((28, 28))   # reshaping the flattened array to so plt can show it
+    #     pltp.imshow(img, cmap='gray')
+    #     pltp.show()
